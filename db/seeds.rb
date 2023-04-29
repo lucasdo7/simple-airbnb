e42242c88b282f38071c8422383837260a13f246
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-29 10:37:45 -0300

    coloquei as gemas de simple form, faker, bootstrap, e fiz a seed dos flats
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating 15 faker flats'
+15.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Restaurant.description,
+    price_per_night: Faker::Number.between(from: 100, to: 500),
+    number_of_guests: Faker::Number.between(from: 2, to: 6),
+    stars: rand(0..5),
+    pet_friendly: Faker::Boolean.boolean(true_ratio: 0.3)
+  )
+  flat.save!
+end
+puts 'Finished!'

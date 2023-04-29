f6106cabe7d9fb51d0ab449691a09eaeb5f8db5b
Lucas Mendes <lucasbiocelp@gmail.com>
2023-04-29 10:54:01 -0300

    adicionei foto e refiz a seed dos flats
    Signed-off-by: Lucas Mendes <lucasbiocelp@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,8 @@ puts 'Creating 15 faker flats'
     price_per_night: Faker::Number.between(from: 100, to: 500),
     number_of_guests: Faker::Number.between(from: 2, to: 6),
     stars: rand(0..5),
-    pet_friendly: Faker::Boolean.boolean(true_ratio: 0.3)
+    pet_friendly: Faker::Boolean.boolean(true_ratio: 0.3),
+    picture_url: ['https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200', 'https://plus.unsplash.com/premium_photo-1678386645963-3f5b0bdb8dcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 'https://images.unsplash.com/photo-1565043589221-1a6fd9ae45c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1897&q=80', 'https://images.unsplash.com/photo-1556784344-ad913c73cfc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80', 'https://images.unsplash.com/photo-1580237072617-771c3ecc4a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'].sample
   )
   flat.save!
 end
